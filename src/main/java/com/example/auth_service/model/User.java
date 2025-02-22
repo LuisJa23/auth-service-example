5e8236538ee576af2dfb418258aa0284b6f9da08
Luis Javier Lopez Galindo <javierlink22@gmail.com>
2025-02-22 11:17:10 -0500

    Configuracion del UserRepository y los Roles en User

--- src/main/java/com/example/auth_service/model/User.java
+++ src/main/java/com/example/auth_service/model/User.java
@@ -2,13 +2,17 @@ package com.example.auth_service.model;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,7 +25,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -50,4 +54,41 @@ public class User {
     private List<Role> roles;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .toList();
+    }
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/auth_service/repository/UserRepository.java
+++ src/main/java/com/example/auth_service/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.auth_service.repository;
 
-public interface UserRepository {
+import com.example.auth_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    //Encuentra un usuario por su correo
+    UserDetails findByUsername(String username);
 }
