2c8db814a25000db1cbdfb6830ece830210621cf
LuisJa23 <126001051+LuisJa23@users.noreply.github.com>
2025-03-11 10:34:05 -0500

    Cambio de puerto

--- src/main/java/com/example/auth_service/infra/security/config/SecurityConfigurations.java
+++ src/main/java/com/example/auth_service/infra/security/config/SecurityConfigurations.java
@@ -51,7 +51,7 @@ public class SecurityConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Permite solicitudes desde React
+        configuration.setAllowedOrigins(List.of("http://0.0.0.0:5173")); // Permite solicitudes desde React
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
